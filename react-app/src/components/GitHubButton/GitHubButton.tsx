fbbd5765a5ecef3f58c69fab55f2b8dfd6a412f9
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-07-03 20:21:58 +0100

    additional changes

--- react-app/src/components/GitHubButton/GitHubButton.tsx
+++ react-app/src/components/GitHubButton/GitHubButton.tsx
@@ -3,9 +3,10 @@ import styles from "./GitHubButton.module.css";
 import { AiFillGithub } from "react-icons/ai";
 
 export default function GitHubButton() {
+	const link = "https://github.com/georgem00re"
 	return (
-		<button className={styles.button}>
+		<a className={styles.button} href={link}>
 			<AiFillGithub size="25px" color="#262626"/>
-		</button>
+		</a>
 	)
 }
--- react-app/src/components/LinkedInButton/LinkedInButton.tsx
+++ react-app/src/components/LinkedInButton/LinkedInButton.tsx
@@ -3,9 +3,10 @@ import styles from "./LinkedInButton.module.css";
 import { BsLinkedin } from "react-icons/bs";
 
 export default function LinkedInButton() {
+	const link = "https://www.linkedin.com/in/george-moore-275087217/"
 	return (
-		<button className={styles.button}>
+		<a className={styles.button} href={link}>
 			<BsLinkedin size="25px" color="#262626"/>
-		</button>
+		</a>
 	)
 }
