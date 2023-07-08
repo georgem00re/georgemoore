78b2d48a9119db28015c13d54ac3567e531fa976
George Moore <114688839+georgem00re@users.noreply.github.com>
2023-07-08 14:59:43 +0100

    Design and write code for React app contact page (#18)

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,14 @@
 import LandingPage from "./pages/LandingPage/LandingPage.tsx";
+import ContactPage from "./pages/ContactPage/ContactPage.tsx";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 export default function App() {
-  return <LandingPage/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/contact" element ={<ContactPage/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
---
+++ react-app/src/components/ContactForm/ContactForm.tsx
@@ -0,0 +1,14 @@
+
+import styles from "./ContactForm.module.css";
+
+export default function ContactForm() {
+	return (
+		<form className={styles.form}>
+			<h1>Get in Touch</h1>
+			<input type="text" placeholder="Name"/>
+			<input type="text" placeholder="Email"/>
+			<textarea placeholder="Message"></textarea>
+			<input type="submit"/>
+		</form>
+	)
+}
